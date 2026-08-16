92e2699db62ddc3cff4ae7090ecab51b2add738d
Otávio Gomes <otaviogomes0303@gmail.com>
2026-08-16 13:54:33 -0300

    Criado a controller

---
+++ src/main/java/dev/java10x/CadastroDeClientes/Controller.java
@@ -0,0 +1,15 @@
+package dev.java10x.CadastroDeClientes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+}
